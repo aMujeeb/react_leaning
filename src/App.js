78b2769244be8ad9c2ed188c395acde0972990e6
Ahamed <mohamed_marzook_ahamed_mujeeb_from.scientec@stb.gov.sg>
2024-06-10 14:11:29 +0800

    Add Main Header

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
-import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
   return (
     <Router>
-      <Switch>
-      <Route path="/" exact>
-          <Users />
-        </Route>
+      <MainNavigation />
+      <main>
+        <Switch>
+        <Route path="/" exact>
+            <Users />
+          </Route>
 
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
 
-        <Redirect to="/" />
-      </Switch>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
